2a71213edf9058227229c8dc0b95df8d115b3206
Praveen <praveenyr@gmail.com>
2017-02-01 00:36:26 +0000

    complete assignment-17-seed-data

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,22 +8,28 @@
 
 require 'random_data'
 
-50. times do
-    Post.create!(
-        title: RandomData.random_sentence,
-        body: RandomData.random_paragraph
-    )
-end
+# 50. times do
+#     Post.create!(
+#         title: RandomData.random_sentence,
+#         body: RandomData.random_paragraph
+#     )
+# end
 
-posts = Post.all
+# posts = Post.all
 
-100. times do
-Comment.create!(
-    post: posts.sample,
-    body: RandomData.random_paragraph
-)
-end
+# 100. times do
+# Comment.create!(
+#     post: posts.sample,
+#     body: RandomData.random_paragraph
+# )
+# end
+  
+puts "#{Post.count} posts created"
+Post.find_or_create_by!(
+    title: "Unique Title",
+    body: "Unique Body"
+    )  
     
 puts "Seed finished"
 puts "#{Post.count} posts created"
-puts "#{Comment.count} comments created"
+# puts "#{Comment.count} comments created"
