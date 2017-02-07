dd27f8b07e40787f1fe33272853e4b640a4544ee
Praveen <praveenyr@gmail.com>
2017-02-07 00:43:06 +0000

    complete checkpoint 20 more-crud

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -17,6 +17,22 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    @post.title = params[:post][:title]
+    @post.body = params[:post][:body] 
+    
+    if @post.save
+      flash[notice] = "Post was saved"
+      redirect_to @post
+    else
+      flash.now[:alert] = "There was an error trying to save the post.Please try again later"
+      render :edit
+    end
   end
   
   def create
@@ -33,4 +49,17 @@ class PostsController < ApplicationController
       render :new
     end
   end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    
+    if @post.destroy
+      flash[:notice] = "\"#{@post.title}\" was deleted successfully. "
+      redirect_to posts_path
+    else
+      flash[:alert] = "There was an error trying to delete the post"
+      render :show
+    end
+  end
+  
 end
