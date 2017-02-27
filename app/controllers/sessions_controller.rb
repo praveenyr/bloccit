44c51d1c885ddea12bd8fb89dc72c06c954b1db2
Brittany Martin <brittany.jill.martin@gmail.com>
2017-02-27 17:26:28 -0500

    Shifted Checkpoint to use Rails 5 syntax, removed merge conflict

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,26 +1,23 @@
 class SessionsController < ApplicationController
-  
   def new
   end
-  
+
   def create
-    binding.pry
     user = User.find_by(email: params[:session][:email].downcase)
-    
+
     if user && user.authenticate(params[:session][:password])
-      create_session(current_user)
-      flash[:notice] = "Welcome #{user.name}"
+      create_session(user)
+      flash[:notice] = "Welcome, #{user.name}!"
       redirect_to root_path
     else
-      flash[:alert] = 'Invalid email/password combination'
+      flash.now[:alert] = 'Invalid email/password combination'
       render :new
     end
   end
-  
+
   def destroy
     destroy_session(current_user)
     flash[:notice] = "You've been signed out, come back soon!"
     redirect_to root_path
   end
-    
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,24 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Topic, type: :model do
-  let(:name){ RandomData.random_sentence}
-  let(:description){ RandomData.random_paragraph}
-  let(:public){ true }
-  let(:topic){ Topic.create!(name: name,description: description)}
-  
-<<<<<<< HEAD
-=======
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:public) { true }
+  let(:topic) { create(:topic) }
+
   it { is_expected.to have_many(:posts) }
-  it { is_expected.to have_many(:sponsoredposts) }
-  
->>>>>>> assignment-22-rails-topicsandposts
+
   describe "attributes" do
-     it "name,description and public attributes" do
-        expect(topic).to have_attributes(name: name, description: description, public: public)
-     end
-    
-     it "is public by default" do
-        expect(topic.public).to be(true)
-     end
-  end    
+    it "has name, description, and public attributes" do
+      expect(topic).to have_attributes(name: topic.name, description: topic.description)
+    end
+
+    it "is public by default" do
+      expect(topic.public).to be(true)
+    end
+  end
 end
